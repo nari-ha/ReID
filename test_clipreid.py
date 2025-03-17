abe476b514cd7baeb074e33fe3f2ae0233f12929
nari-ha <narihanar@gmail.com>
2025-03-17 16:36:12 +0000

    asdfds

--- test_clipreid.py
+++ test_clipreid.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     os.environ['CUDA_VISIBLE_DEVICES'] = cfg.MODEL.DEVICE_ID
 
     train_loader, train_loader_normal, val_loader, num_query, num_classes, camera_num, view_num = make_dataloader(cfg)
-
+    print("num_query: ", num_query, "num_classes: ", num_classes)
     model = make_model(cfg, num_class=num_classes, camera_num=camera_num, view_num = view_num)
     model.load_param(cfg.TEST.WEIGHT)
 
